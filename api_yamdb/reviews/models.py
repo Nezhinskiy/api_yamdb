1014474f8dea62046c5ef2406f94ab86c75f9659
Mikhail Nezhinsky <m.nezhinsky@yandex.ru>
2022-08-08 20:03:39 +0300

    add Reviews model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+SCORES = (
+    (1, 'Пришлось соврать, что смотрю порно, когда мама вошла в комнату'),
+    (2, 'Каждый в моей комнате стал тупее'),
+    (3, 'Лучше б я этого не видел'),
+    (4, '3,6 - не отлично, но и не ужасно.'),
+    (5, 'Видали мы и по-лучше'),
+    (6, 'Потенциал не раскрыт'),
+    (7, 'Пересматривать бы не стал'),
+    (8, 'Моё уважение'),
+    (9, 'Я ничего не понял, но сделаю вид, что всё понял'),
+    (10, 'Рыдала вся маршрутка')
+)
+
+
+class Title(models.Model):
+    pass
+
+
+class Reviews(models.Model):
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение',
+        help_text='Произведение, к которой будет относиться ревью',
+    )
+    text = models.TextField(verbose_name='Текст ревью',
+                            help_text='Текст нового ревью')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор',
+    )
+    score = models.IntegerField(choices=SCORES, verbose_name='Баллы')
+    pup_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации',
+        db_index=True,
+    )
