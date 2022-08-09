2b7a8b912ce40bf1a019901b91df9ba88c4364de
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2022-08-09 20:54:42 +0400

    Added users endpoint

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import GetTokenView, SignUpView
+from . import views
 
 app_name = 'api'
 
-api_router = DefaultRouter()
+router_v1 = DefaultRouter()
+router_v1.register('users', views.UserViewSet, basename='user')
 
 urlpatterns = [
-    path('v1/auth/signup/', SignUpView.as_view(), name='auth_signup'),
-    path('v1/auth/token/', GetTokenView.as_view(), name='auth_token'),
+    path('v1/auth/signup/', views.SignUpView.as_view(), name='auth_signup'),
+    path('v1/auth/token/', views.GetTokenView.as_view(), name='auth_token'),
+    path('v1/users/me/', views.CurrentUserView.as_view(), name='current_user'),
+    path('v1/', include(router_v1.urls)),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,20 @@
-from rest_framework import permissions, status
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions, status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import SignUpSerializer, TokenSerializer
+from . import serializers
+from .permissions import IsAdministrator
 from .utils import get_token_for_user
+
+User = get_user_model()
 
 
 class SignUpView(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request):
-        serializer = SignUpSerializer(data=request.data)
+        serializer = serializers.SignUpSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -21,9 +25,27 @@ class GetTokenView(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request):
-        serializer = TokenSerializer(data=request.data)
+        serializer = serializers.TokenSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             return Response(get_token_for_user(user),
                             status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+    permission_classes = (IsAdministrator,)
+    lookup_field = 'username'
+
+
+class CurrentUserView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def perform_update(self, serializer):
+        serializer.save(role=self.request.user.role)
