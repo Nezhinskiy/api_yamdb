30e08c1ea0ef4c7f1872029f5435014ddd499c5f
Mikhail Nezhinsky <m.nezhinsky@yandex.ru>
2022-08-09 20:13:44 +0300

    rename migrations and models add and register api urls add ReviewViewSet and CommentViewSet add ReviewSerializer and CommentSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
+from reviews.models import Comment, Review
+
 User = get_user_model()
 
 
@@ -53,3 +55,22 @@ class TokenSerializer(serializers.Serializer):
                 {'confirmation_code': 'Неверный код подтверждения!'}
             )
         return data
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('review_id',)
