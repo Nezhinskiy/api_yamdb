82a9168cd61d873b3a81310876da4bdbcfa64c0b
Mikhail Nezhinsky <m.nezhinsky@yandex.ru>
2022-08-11 15:25:36 +0300

    Добавил сортировку в User Обновил миграции Рефакторинг permission

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -39,6 +39,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ['date_joined']
 
     @property
     def is_user(self):
